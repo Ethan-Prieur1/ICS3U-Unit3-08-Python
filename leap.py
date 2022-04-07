9923a77155c88b8bb5c12a08ca13d9cbef62c94f
Ubuntu <ubuntu@ip-172-31-41-46.ec2.internal>
2022-04-07 18:20:01 +0000

    Added leap year file

--- leap.py
+++ leap.py
@@ -9,21 +9,26 @@ def main():
     # This function is the main program
 
     # Input
-    year = int(input("Enter a Year: "))
+    year_as_string = input("Enter a Year: ")
 
     # Process & output
 
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                print("That IS a Leap Year! YAHOO!")
+    try:
+        year_as_int = int(year_as_string)
+        if year_as_int % 4 == 0:
+            if year_as_int % 100 == 0:
+                if year_as_int % 400 == 0:
+                    print("That IS a Leap Year! YAHOO!")
+                else:
+                    print("Aw Man, That's NOT a Leap Year!")
             else:
-                print("Aw Man, That's NOT a Leap Year!")
+                print("That IS a Leap Year! YAHOO!")
         else:
-            print("That IS a Leap Year! YAHOO!")
-    else:
-        print("Aw Man, That's NOT a Leap Year!")
-    print("\nDone.")
+            print("Aw Man, That's NOT a Leap Year!")
+    except Exception:
+        print("That is not an Integer")
+    finally:
+        print("\nDone.")
 
 
 if __name__ == "__main__":
